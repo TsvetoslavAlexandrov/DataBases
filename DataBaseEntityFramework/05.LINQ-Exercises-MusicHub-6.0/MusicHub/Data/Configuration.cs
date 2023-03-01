6dde74bb262ac0ab32643a0cb029fab65eb827f3
Tsvetoslav Alexandrov <106446104+TsvetoslavAlexandrov@users.noreply.github.com>
2023-03-01 23:30:21 +0200

    Add files via upload

---
+++ DataBaseEntityFramework/05.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace MusicHub.Data;
+
+public static class Configuration
+{
+    public static string ConnectionString =
+        @"Server=TSVTOSLAV\SQLEXPRESS;Database=MusicHub;Integrated Security=True;Encrypt=False;";
+}
---
+++ DataBaseEntityFramework/05.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/ValidationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicHub.Data;
+
+public static class ValidationConstants
+{
+    public const int SongNameLength = 20;
+    public const int AlbumNameLength = 40;
+    public const int PerformarNameLength = 20;
+    public const int ProducerNameLength = 30;
+    public const int WriterNameLength = 20;
+}
